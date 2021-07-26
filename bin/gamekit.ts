f52ad679154e1f90933f1fedf068f9ac1a2c5df4
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-07-26 15:50:31 -0400

    add dependencies

--- bin/gamekit.ts
+++ bin/gamekit.ts
@@ -2,15 +2,16 @@
 "use strict";
 const { Command } = require("commander");
 const spawn = require("cross-spawn");
+const packageJson = require("../package.json");
 
 const args = process.argv.slice(2);
-const program = new Command().version("1.0.0");
+const program = new Command().version(packageJson.version);
 
 program
   .command("start")
   .description("starts a development server")
   .option("-p, --port <port_number>", "1234")
-  .action((options) => {});
+  .action((options: any) => {});
 
 program
   .command("build")
