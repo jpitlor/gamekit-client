58acd68502b26466b10aa4a29cfe8863724e5f20
Jordan Pitlor <jordan@pitlor.dev>
2021-08-12 22:19:27 -0400

    add profile functions

--- src/profile.ts
+++ src/profile.ts
@@ -1,1 +1,22 @@
-export function placeholder() {}
+import { createAvatar } from "@dicebear/avatars";
+import * as sprites from "@dicebear/avatars-human-sprites";
+import { colors, animals, uniqueNamesGenerator } from "unique-names-generator";
+
+export function asImage(avatar: string): string {
+  return createAvatar(sprites, {
+    seed: avatar,
+    width: 150,
+    height: 150,
+    margin: 15,
+    dataUri: true,
+  });
+}
+
+export function getRandomName(): string {
+  return uniqueNamesGenerator({
+    dictionaries: [colors, animals],
+    length: 2,
+    separator: " ",
+    style: "capital",
+  });
+}
